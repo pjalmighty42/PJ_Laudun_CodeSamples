41ddb0f2c05fc936b3dce5b69f1708aff1283fb3
PJ Laudun <pjalmighty42@users.noreply.github.com>
2021-10-09 10:52:19 -0400

    Delete WebDev/FrontEnd/epic-coding-challenge directory

--- WebDev/FrontEnd/epic-coding-challenge/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import listReducer from "./features/listSlice";
-
-//Create the reducer and store
-export const store = configureStore({
-  reducer: {
-    list: listReducer
-  }
-});
