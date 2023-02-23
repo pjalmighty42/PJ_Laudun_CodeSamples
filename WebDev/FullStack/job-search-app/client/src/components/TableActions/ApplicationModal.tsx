dc39434ac5d33cc799e5aeb19ee04fe6cb6e826b
PJ Laudun <pjalmighty42@users.noreply.github.com>
2023-02-23 14:41:15 -0500

    modal done

--- WebDev/FullStack/job-search-app/client/src/components/TableActions/ApplicationModal.tsx
+++ WebDev/FullStack/job-search-app/client/src/components/TableActions/ApplicationModal.tsx
@@ -1,3 +1,6 @@
+import { ReactElement, useState } from "react";
+import { useMutation } from "@apollo/client";
+
 import {
   Dialog,
   DialogTitle,
@@ -5,10 +8,24 @@ import {
   DialogContentText,
   DialogActions,
   Button,
+  TextField,
+  Stack,
+  Select,
+  MenuItem,
+  FormControl,
+  SelectChangeEvent,
+  Checkbox,
+  Box,
 } from "@mui/material";
 import ApplicationInterface from "../../interfaces/ApplicationInterfaces";
 import { styled } from "@mui/material/styles";
-import { yellow, green } from "@mui/material/colors";
+import { yellow, green, grey } from "@mui/material/colors";
+import { Label } from "@mui/icons-material";
+
+const ColumnBox = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+});
 
 const DialogHeaderEdit = styled(DialogTitle)({
   backgroundColor: yellow[100],
@@ -22,6 +39,15 @@ const DialogHeaderNew = styled(DialogTitle)({
   fontWeight: "bold",
 });
 
+const DialogSubHeader = styled(DialogContentText)({
+  marginTop: "1em",
+  marginBottom: ".5em",
+  borderBottom: "2px solid",
+  borderColor: grey[500],
+  fontSize: "1.25em",
+  fontWeight: "bold",
+});
+
 interface ApplicationModalProps {
   isOpen: boolean;
   isEdit: boolean;
@@ -29,7 +55,35 @@ interface ApplicationModalProps {
   setIsOpen: () => void;
 }
 
+interface FieldGeneratorProps {
+  children: Array<ReactElement>;
+  width: string;
+}
+
+const FormFieldGenerator = ({ children, width = "" }: FieldGeneratorProps) => {
+  return <ColumnBox width={width}>{children}</ColumnBox>;
+};
+
 function ApplicaionModal({ isOpen, isEdit, setIsOpen }: ApplicationModalProps) {
+  const [application, setApplication] = useState<ApplicationInterface>({
+    id: "",
+    companyName: "",
+    dateApplied: "",
+    role: {
+      id: "",
+      name: "",
+      url: "",
+    },
+    status: "",
+    submittedResume: false,
+    resumeViewed: false,
+    contacted1stCall: false,
+    techInterview: false,
+    interview3: false,
+    interview4: false,
+    jobOffered: false,
+  });
+
   return (
     <>
       <Dialog
@@ -46,7 +100,92 @@ function ApplicaionModal({ isOpen, isEdit, setIsOpen }: ApplicationModalProps) {
             Create New Application
           </DialogHeaderNew>
         )}
-        <DialogContent></DialogContent>
+        <DialogContent id="modal-content">
+          <DialogSubHeader>Basic Information</DialogSubHeader>
+          <Stack direction="row" spacing={2} id="application-basic-info">
+            <FormFieldGenerator width="33%">
+              <label>Company Name</label>
+              <TextField type="text" variant="outlined" size="small" />
+            </FormFieldGenerator>
+            <FormFieldGenerator width="33%">
+              <label>Date Applied</label>
+              <TextField type="date" variant="outlined" size="small" />
+            </FormFieldGenerator>
+            <FormFieldGenerator width="33%">
+              <label>Status</label>
+              <FormControl size="small">
+                <Select value={"none"}>
+                  <MenuItem value="none">Please Select a Status</MenuItem>
+                  <MenuItem value="active">Active</MenuItem>
+                  <MenuItem value="hold">On Hold</MenuItem>
+                  <MenuItem value="rejected">Rejected</MenuItem>
+                </Select>
+              </FormControl>
+            </FormFieldGenerator>
+          </Stack>
+          <DialogSubHeader>Role Information</DialogSubHeader>
+          <Stack direction="row" spacing={2} id="application-role-info">
+            <FormFieldGenerator width="50%">
+              <label>Role Name</label>
+              <TextField type="text" variant="outlined" size="small" />
+            </FormFieldGenerator>
+            <FormFieldGenerator width="50%">
+              <label>Job Description URL</label>
+              <TextField type="text" variant="outlined" size="small" />
+            </FormFieldGenerator>
+          </Stack>
+          <DialogSubHeader>Current Status Checks</DialogSubHeader>
+          <Stack direction="row" spacing={2} id="application-role-info">
+            <FormFieldGenerator width="14%">
+              <label>Submitted Resume?</label>
+              <Checkbox inputProps={{ "aria-label": "Submitted Resume?" }} />
+            </FormFieldGenerator>
+            <FormFieldGenerator width="14%">
+              <label>Resume Reviewed?</label>
+              <Checkbox inputProps={{ "aria-label": "Was Resume Reviewed?" }} />
+            </FormFieldGenerator>
+            <FormFieldGenerator width="14%">
+              <label>Initial Call?</label>
+              <Checkbox
+                inputProps={{
+                  "aria-label": "Did you receive an Initial Phone Call?",
+                }}
+              />
+            </FormFieldGenerator>
+            <FormFieldGenerator width="14%">
+              <label>Tech Interview?</label>
+              <Checkbox
+                inputProps={{
+                  "aria-label": "Did you do a Tech Interview?",
+                }}
+              />
+            </FormFieldGenerator>
+            <FormFieldGenerator width="14%">
+              <label>3rd Interview?</label>
+              <Checkbox
+                inputProps={{
+                  "aria-label": "Did you have a 3rd Interview?",
+                }}
+              />
+            </FormFieldGenerator>
+            <FormFieldGenerator width="14%">
+              <label>4th Interview?</label>
+              <Checkbox
+                inputProps={{
+                  "aria-label": "Did you have a 4rd Interview?",
+                }}
+              />
+            </FormFieldGenerator>
+            <FormFieldGenerator width="14%">
+              <label>Job Offer?</label>
+              <Checkbox
+                inputProps={{
+                  "aria-label": "Did you receive a Job Offer?",
+                }}
+              />
+            </FormFieldGenerator>
+          </Stack>
+        </DialogContent>
         <DialogActions style={{ marginBottom: "5px" }}>
           <Button
             onClick={setIsOpen}
